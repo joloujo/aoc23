a071af6063205c8e039499123acb7ad76e3a4a62
Dominic Salmieri <dsalmieri@olin.edu>
2023-12-02 00:41:16 -0500

    Day 2 cleanup

--- days/2.2.py
+++ days/2.2.py
@@ -10,8 +10,10 @@ answer = 0
 for raw_line in data:
     line = raw_line.strip('\n\r')
     
+    # split the line into rounds
     rounds = line.split(';')
     
+    # Create a dictionary of the minimum number of each color to make the round possible
     min_color = {
         "red": 0,
         "green": 0,
@@ -20,30 +22,34 @@ for raw_line in data:
 
     for round in rounds:
 
+        # split the round by spaces
         words = round.split()
 
-        id: int
+        # If the first word is "Game", then the round is the first one in the game
         if words[0] == "Game":
-            id = int(words[1][0:-1])
-            # print(id)
-
+            # Remove the first two words ("Game" and the id) so that the rest of the words are the same as the other rounds
             words.pop(0)
             words.pop(0)
 
-        # print(words)
+        # print("DEBUG words:", words)
 
+        # For each pair of words,
         for i in range(0, int(len(words)/2)):
-            number = int(words[2 * i])
-            color = words[2 * i + 1]
+            number = int(words[2 * i]) # The number is the first word of the pair
+            color = words[2 * i + 1] # The color is the second word of the pair
 
+            # Remove the comma or semicolon from the color if it exists
             if ',' in color or ';' in color:
                 color = color[0:-1]
 
+            # If the number is greater than the current minimum number of that color, then update the minimum number
             if number > min_color[color]:
                 min_color[color] = number
     
+    # Add the "power" of the round (the product of the minimum numbers) to the answer
     answer += min_color['red'] * min_color['green'] * min_color['blue']
     print(min_color)
     print(answer)
 
+# print the answer
 print(answer)
--- days/2.py
+++ days/2.py
@@ -6,6 +6,7 @@ TEST = False
 
 data = loadDay(DAY, TEST)
 
+# Create a dictionary of the maximum number of each color
 max_color = {
     "red": 12,
     "green": 13,
@@ -16,36 +17,45 @@ answer = 0
 for raw_line in data:
     line = raw_line.strip('\n\r')
     
+    # split the line into rounds
     rounds = line.split(';')
 
+    # Create a boolean to check if any round is possible
     possible = True
     for round in rounds:
 
+        # split the round by spaces
         words = round.split()
 
-        id: int
+        # If the first word is "Game", then the round is the first one in the game
         if words[0] == "Game":
+            # Get the id of the game
             id = int(words[1][0:-1])
             # print(id)
 
+            # Remove the first two words ("Game" and the id) so that the rest of the words are the same as the other rounds
             words.pop(0)
             words.pop(0)
 
-        # print(words)
+        # print("DEBUG words:", words)
 
+        # For each pair of words,
         for i in range(0, int(len(words)/2)):
-            number = int(words[2 * i])
-            color = words[2 * i + 1]
+            number = int(words[2 * i]) # The number is the first word of the pair
+            color = words[2 * i + 1] # The color is the second word of the pair
 
+            # Remove the comma or semicolon from the color if it exists
             if ',' in color or ';' in color:
                 color = color[0:-1]
 
+            # If the number is greater than the maximum number of that color, then the round is not possible
             if number > max_color[color]:
                 possible = False
-                print 
-                break
+                # print("DEBUG impossible id:", id)
+                break # for efficiency
 
     if possible:
         answer += id # type: ignore
     
+# print the answer
 print(answer)
