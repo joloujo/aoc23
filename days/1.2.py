0c86a00a0d918d37f698ec351a90777e3f72aa27
Dominic Salmieri <dsalmieri@olin.edu>
2023-12-01 20:22:04 -0500

    Day 1 cleanup and comments

--- days/1.2.py
+++ days/1.2.py
@@ -6,6 +6,7 @@ TEST = False
 
 data = loadDay(DAY, TEST)
 
+# create a dictionary of the digits to look for and their values
 digits= {
     '0': 0,
     '1': 1,
@@ -29,35 +30,42 @@ digits= {
     'nine': 9
 }
 
-print(digits.keys())
+# print("DEBUG digits keys:", digits.keys())
 
+# create a list to store each line of the data file with no letters
 no_letters = []
 
-answer = 0
+answer = 0 # start the sum for the answer at 0
 for raw_line in data:
     line = raw_line # .strip('\n\r')
 
+    # make lists for the first and last occurences of each digit
     indexes_list = []
     rindexes_list = []
+
+    # find the first and last occurences of each digit
     for digit in digits.keys():
         indexes_list.append(line.find(digit))
         rindexes_list.append(line.rfind(digit))
 
+    # find the first occurence of any digit
     indexes = np.array(indexes_list)
     if np.any(indexes == -1):
         indexes[indexes == -1] = 1000000
     first_index = np.argmin(indexes)
 
+    # find the last occurence of any digit
     rindexes = np.array(rindexes_list)
     last_index = np.argmax(rindexes)
 
+    # get the value of the first and last digits
     first_num = list(digits.values())[first_index] # type: ignore
     last_num = list(digits.values())[last_index] # type: ignore
 
-    print(first_num, last_num)
+    # print("DEBUG first and last number:", first_num, last_num)
 
+    # create a two-digit number from the first and last digits and add it to the answer
     answer += first_num * 10 + last_num
 
+# display the answer
 print(answer)
-
-# not 54225
--- days/1.py
+++ days/1.py
@@ -6,20 +6,30 @@ TEST = False
 
 data = loadDay(DAY, TEST)
 
-digits= ['0','1','2','3','4','5','6','7','8','9','zero','one','two','three','four','five','six','seven','eight','nine']
+# create a list of the digits to look for
+digits= ['0','1','2','3','4','5','6','7','8','9']
 
+# create a list to store each line of the data file with no letters
 no_letters = []
 
-answer = 0
 for raw_line in data:
     line = raw_line.strip('\n\r')
+
+    # add an empty string to the no_letters list
     no_letters.append('')
+
+    # add each character that is in the digits list to the last item in the no_letters list
     for char in line:
         if char in digits:
             no_letters[-1] = no_letters[-1] + char
 
-for thing in no_letters:
-    num = int(thing[0] + thing[-1])
+answer = 0 # start the sum for the answer at 0
+for char in no_letters:
+    # concatenate the first and last digits to make the two-digit number
+    num = int(char[0] + char[-1])
+
+    # add the two-digit number to the answer
     answer += num
 
+# display the answer
 print(answer)
