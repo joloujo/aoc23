044af57a5584872fe8f88d9ddee7f88b9695bb07
Dominic Salmieri <dsalmieri@olin.edu>
2023-12-09 00:25:28 -0500

    Day 7

---
+++ days/7.1.py
@@ -0,0 +1,66 @@
+from updateData import loadDay # Automatically updates data files
+import numpy as np
+
+DAY = 7
+TEST = False
+
+data = loadDay(DAY, TEST)
+if data == None:
+    print("No data yet :(")
+    exit()
+
+card_strength = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+
+def get_type_rank(hand):
+    cards = dict()
+    for card in hand:
+        if card in cards:
+            cards[card] += 1
+        else:
+            cards[card] = 1  
+
+    if len(list(cards.keys())) == 1:
+        # 5 of a kind
+        return 6
+    elif len(list(cards.keys())) == 2:
+        # 4 of a kind or full house
+        for value in list(cards.keys()):
+            if cards[value] == 4:
+                # 4 of a kind
+                return 5
+        return 4
+    elif len(list(cards.keys())) == 3:
+        # 3 of a kind or 2 pair
+        for value in list(cards.keys()):
+            if cards[value] == 3:
+                # 3 of a kind
+                return 3
+        # two pair
+        return 2
+    elif len(list(cards.keys())) == 4:
+        return 1
+    else:
+        return 0
+
+def hand_to_strength(hand):
+
+    multiplier_step = 100
+    multiplier = pow(multiplier_step, len(hand))
+
+    strength = get_type_rank(hand) * multiplier
+    multiplier /= multiplier_step
+
+    for card in hand:
+        strength += card_strength.index(card) * multiplier
+        multiplier /= multiplier_step
+
+    return strength
+
+lines = data.readlines()
+
+
+strengths_and_bids = [(hand_to_strength(line.strip('\n\r').split()[0]), int(line.strip('\n\r').split()[1])) for line in lines]
+strengths_and_bids.sort(key=lambda x: x[0])
+multiplied_bids = [sab[1] * (i+1) for i, sab in enumerate(strengths_and_bids)]
+
+print(sum(multiplied_bids))
